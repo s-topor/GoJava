aff4aed7cf6d778a7fed749f49744bc21eae4c11
s-topor <sergey.toporko@gmail.com>
2016-05-25 12:20:30 +0300

    first commit

---
+++ src/com/student/gojava/module01/FirstProgram.java
@@ -0,0 +1,10 @@
+package com.student.gojava.module01;
+
+/**
+ * Created by toporko on 25.05.2016.
+ */
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
